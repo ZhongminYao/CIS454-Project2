903f8d0f4a219f2a6e831eeed9c96647471fb773
Alan Yao <zyao02@syr.edu>
2019-04-08 23:47:51 -0400

    google sign-in method implemented

--- public/signin.js
+++ public/signin.js
@@ -13,6 +13,26 @@
   if (!firebase.apps.length) {
     firebase.initializeApp({config});
  }
+  
+  // configure by firebaseUI
+  /*var UIconfig = {
+    signInSuccessUrl: 'AccountInformation.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthPro87vider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    ],
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    //tosUrl: '<your-tos-url>',
+    // Privacy policy url/callback.
+    //privacyPolicyUrl: function() {
+      //window.location.assign('<your-privacy-policy-url>');
+    //}
+    
+}; */
   // read the web elements
   var input_email = document.getElementById('email');
   var input_password = document.getElementById('password');
@@ -39,13 +59,28 @@
     action.catch(e => console.log(e.message));
   });
 
+  
+  /*
+  function authStateObserver(user) {
+    if (user) {
+      window.location = ("./AccountInformation.html");
+    }
+  }
+
+  
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser) {
       console.log(firebaseUser);
+      onSignIn(firebaseUser);
+      onSuccess(firebaseUser);
     } else {
       console.log('not logged in');
     }
   }
+  
     )
+    */
+
+    
 
 }());
